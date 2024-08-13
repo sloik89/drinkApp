97bc59ffc1e7683fda3b41141f96bc3724f8bef9
sloik89 <sloik89@gmail.com>
2024-08-13 15:17:47 +0200

    add structure html

--- src/pages/Cocktail.tsx
+++ src/pages/Cocktail.tsx
@@ -1,9 +1,9 @@
-import { useLoaderData, type LoaderFunction } from "react-router-dom";
+import { useLoaderData, type LoaderFunction, Link } from "react-router-dom";
 import { customFetch } from "../utilis/customFetch";
 import { type Drink } from "../utilis/types";
 export const loader: LoaderFunction = async ({ params }): Promise<null> => {
   const { id } = params;
-  console.log(id);
+
   try {
     const res = await customFetch(`lookup.php?i=${id}`);
     console.log(res);
@@ -23,8 +23,54 @@ const Cocktail = () => {
       ingrident.push(drink[prop]);
     }
   }
-  console.log(ingrident);
-  return <div>Cocktail</div>;
+  console.log(drink);
+  const rensderDrink = {
+    id: drink.idDrink,
+    name: drink.strDrink,
+    img: drink.strDrinkThumb,
+    category: drink.strCategory,
+    glass: drink.strGlass,
+    info: drink.strAlcoholic,
+    instructions: drink.strInstructions,
+  };
+  console.log(rensderDrink);
+  return (
+    <div className="page">
+      <header>
+        <button>
+          <Link to="/">Go back</Link>
+        </button>
+        <h3>{rensderDrink.name}</h3>
+        <img
+          className="drink-img"
+          src={rensderDrink.img}
+          alt={rensderDrink.name}
+        />
+      </header>
+      <div className="drink-info">
+        <p>
+          <span>Name: </span>
+          {rensderDrink.name}
+        </p>
+        <p>
+          <span>Category: </span>
+          {rensderDrink.category}
+        </p>
+        <p>
+          <span>Glass: </span>
+          {rensderDrink.glass}
+        </p>
+        <p>
+          <span>Info: </span>
+          {rensderDrink.info}
+        </p>
+        <p>
+          <span>Instructions: </span>
+          {rensderDrink.instructions}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Cocktail;
